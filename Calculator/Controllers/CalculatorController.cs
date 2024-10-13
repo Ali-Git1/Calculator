cb100cf76726291a271ca9a7f106feceb083db95
Ali Aliyev <elvinaliyev724@gmail.com>
2024-10-13 22:50:26 +0400

    calculator

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(CalculatorModel model)
+        {
+            try
+            {
+                // Kullanıcıdan alınan ifadeyi hesapla
+                var result = new DataTable().Compute(model.Expression, null);
+                model.Result = Convert.ToDouble(result);
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Geçersiz ifade. Lütfen doğru bir matematiksel ifade girin.");
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public string Expression { get; set; } // Kullanıcıdan alınacak matematiksel ifade
+        public double Result { get; set; } // Hesaplanan sonuç
+    }
+}
